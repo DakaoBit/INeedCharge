bef9d4f792a4d598001d35ad862272bd0946cb7b
StanWang <StanWang@jquan.com.tw>
2024-03-30 08:04:01 +0800

    init

---
+++ INeedCharge/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace INeedCharge.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
