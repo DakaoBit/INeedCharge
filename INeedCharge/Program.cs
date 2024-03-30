984e19056a9980ffde559391f2bad208dd91f3ae
StanWang <StanWang@jquan.com.tw>
2024-03-30 08:46:44 +0800

    adjust Program.cs - setting log

--- INeedCharge/Program.cs
+++ INeedCharge/Program.cs
@@ -1,27 +1,88 @@
-var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddControllersWithViews();
+using Serilog.Events;
+using Serilog.Filters;
+using Serilog;
 
-var app = builder.Build();
+#region Logger Configuration
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+/** Serilog File Setting
+ * 
+ * ★ File Limit:
+ *1. fileSizeLimitBytes: limits file size to 1GB by default，若要移除限制 => fileSizeLimitBytes: null
+ *2. retainedFileCountLimit:  the most recent 31 files are retained by default (Day)，若要移除限制=> retainedFileCountLimit: null
+ *
+ *Rolling policies:
+ *1. rollOnFileSizeLimit: To roll when the file reaches fileSizeLimitBytes, 
+       specify rollOnFileSizeLimit => rollOnFileSizeLimit: true
+ *
+ *JSON event formatting:
+ *1. exapmle => .WriteTo.File(new CompactJsonFormatter(), "log.txt")
+ *
+ */
+
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Debug()
+    .MinimumLevel.Override("Microsoft", LogEventLevel.Debug)
+    .MinimumLevel.Override("Microsoft.AspNetCore", LogEventLevel.Debug)
+    .Enrich.FromLogContext()
+    .Enrich.WithThreadId()
+    .Enrich.WithThreadName()
+    .WriteTo.Console(outputTemplate: "[{Timestamp: HH:mm:ss} {Level:u3}] {Message:lj} <s:{SourceContext}>{NewLine}{Exception}") //Log 寫入Console
+    //log統一在一個file
+    //.WriteTo.File("Logs/log.txt", rollingInterval: RollingInterval.Day, retainedFileCountLimit: 31, outputTemplate: "[{Timestamp:yyyy-MM-dd HH:mm:ss.fff} {Level:u3}]  {Message:lj} <s:{SourceContext}>{NewLine}{Exception}")
+    //log分級分開寫入不同file
+    .WriteTo.Logger(lc => lc
+        .Filter.ByIncludingOnly(e => e.Level == LogEventLevel.Information)
+        .WriteTo.File("Logs/LogInformation/LogInformation-.txt", rollingInterval: RollingInterval.Hour, retainedFileCountLimit: 24 * 7, outputTemplate: "[{Timestamp: HH:mm:ss} {Level:u3}]<{ThreadId}> {Message:lj} <s:{SourceContext}>{NewLine}{Exception}"))
+    .WriteTo.Logger(lc => lc
+        .Filter.ByIncludingOnly(e => e.Level == LogEventLevel.Error)
+        .WriteTo.File("Logs/LogError/LogError-.txt", rollingInterval: RollingInterval.Hour, retainedFileCountLimit: 24 * 7, outputTemplate: "[{Timestamp: HH:mm:ss} {Level:u3}]<{ThreadId}> {Message:lj} <s:{SourceContext}>{NewLine}{Exception}"))
+    .WriteTo.Logger(lc => lc
+        .Filter.ByIncludingOnly(e => e.Level == LogEventLevel.Warning)
+        .WriteTo.File("Logs/LogWarning/LogWarning-.txt", rollingInterval: RollingInterval.Hour, retainedFileCountLimit: 24 * 7, outputTemplate: "[{Timestamp: HH:mm:ss} {Level:u3}]<{ThreadId}> {Message:lj} <s:{SourceContext}>{NewLine}{Exception}"))
+   .Filter.ByExcluding(Matching.FromSource("Quartz"))
+   .CreateLogger();
+#endregion
+
+try
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    Log.Information("Starting Web Application");
+
+    var builder = WebApplication.CreateBuilder(args);
+    builder.Host.UseSerilog();
+
+    // Add services to the container.
+    builder.Services.AddControllersWithViews();
+
+    var app = builder.Build();
+
+    // Configure the HTTP request pipeline.
+    if (!app.Environment.IsDevelopment())
+    {
+        app.UseExceptionHandler("/Home/Error");
+        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+        app.UseHsts();
+    }
+
+    app.UseHttpsRedirection();
+    app.UseStaticFiles();
+
+    app.UseRouting();
+
+    app.UseAuthorization();
+
+    app.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}");
+
+    app.Run();
+
 }
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
-
-app.Run();
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Application terminated unexpectedly");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
